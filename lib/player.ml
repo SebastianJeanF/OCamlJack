43c8a48c32d4b21930486f8bfdde34f4a5c99407
Sebastian Jean-Francois <sj728@cornell.edu>
2024-04-25 12:47:27 -0400

    Implement feature for a Player to place/win/loss bet - Created place_bet function - Created update_balance function

--- lib/player.ml
+++ lib/player.ml
@@ -4,17 +4,18 @@ type player = {
   name : string;
   hand : card list;
   balance : int;
+  bet : int;
 }
 
 let max_value = 21
 let get_balance p = p.balance
 let get_hand p = p.hand
 let get_name p = p.name
-let create name = { name; hand = []; balance = 0 }
+let create name = { name; hand = []; balance = 0; bet = 0 }
 
 let add_card card p =
   match p with
-  | { name; hand; balance } -> { name; hand = card :: hand; balance }
+  | { name; hand; balance; bet } -> { name; hand = card :: hand; balance; bet }
 
 let get_hand_value p =
   let rec helper hand value aces_count =
@@ -32,5 +33,15 @@ let get_hand_value p =
         else helper t (value + min rank 10) aces_count
   in
   helper p.hand 0 0
+
+let update_balance is_win = function
+  | { name; hand; balance; bet } ->
+      let new_balance = if is_win then balance + (bet * 2) else balance in
+      { name; hand; balance = new_balance; bet = 0 }
+
+let place_bet amount = function
+  | { name; hand; balance; _ } ->
+      let new_balance = balance - amount in
+      { name; hand; balance = new_balance; bet = amount }
 
 let is_bust p = get_hand_value p > max_value
--- lib/player.mli
+++ lib/player.mli
@@ -15,6 +15,12 @@ val get_hand : player -> card list
 (** [get_name player] is the name of [player] *)
 val get_name : player -> string
 
+(** [place_bet x] is a player who placed a bet value [x] in a game. *)
+val place_bet : int -> player -> player
+
+(** [update_balance is_win] is a player who won twice their 
+    bet if [is_win] is true, and lost their bet if [is_win] is false *)
+val update_balance : bool -> player -> player
 
 (** [add_card card player] is a [player] given an additional [card] *)
 val add_card : card -> player -> player
